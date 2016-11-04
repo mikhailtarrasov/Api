83037d8aa6b79e687b55e661ca8da39403444b92
mozard742 <mikhailtarrasov@gmail.com>
2016-11-04 18:19:18 +0500

    Добавление MVC проекта

---
+++ MvcApplication1/MvcApplication1/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        // ViewResult             - поручаем MVC обработать представление и вернуть HTML
+        // RedirectResult         - заставляем браузер перенаправиться на другой сайт
+        // HttpUnauthorizedResult - заставляем пользователя залогиниться
+        // Эти объекты - результаты действия и происходят из класса ActionResult
+
+        public ViewResult Index()   /*Создаем ВьюРезалт, вызывая метод Вью*/
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+
+            return View();
+        }
+        //public ActionResult Index()               
+        //{
+        //    return View();
+        //}
+
+    }
+}
